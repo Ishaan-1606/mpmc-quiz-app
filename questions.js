0482d15ced0f550b2917548043d10cd91f64d4f9
Ishaan-1606 <150211401+Ishaan-1606@users.noreply.github.com>
2023-11-09 11:14:57 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions=[
+    {
+        numb:1,
+        question:"8051 series has how many 16 bit registers?",
+        answer:"a) 2",
+        options:[
+            "a) 2",
+            "b) 3",
+            "c) 1",
+            "d) 0",
+        ]
+    },
+    {
+        numb:2,
+        question:"How many bytes of bit addressable memory is present in 8051 based microcontrollers?",
+        answer:"c) 16 bytes",
+        options:[
+            "a) 8 bytes",
+            "b) 32 bytes",
+            "c) 16 bytes",
+            "d)128 bytes"
+        ]
+    },
+    {
+        numb:3,
+        question:")8051 can access up to_____ of program memory and _____of external data",
+        answer:"3. 64 KB, 64 KB",
+        options:[
+           "1. 64 KB, 32 KB",
+            "2. 32 KB, 64 KB",
+           "3. 64 KB, 64 KB",
+           "4. 16 KB, 64 KB"
+        ]
+    },    
+    {
+        numb:4,
+        question:"The function of ALE signal in 8051 microcontroller is",
+        answer:"2. To control demultiplexing the address and data bus.",
+        options:[
+           "1. To extend the memory capacity",
+           "2. To control demultiplexing the address and data bus.",
+            "3. To distinguish between external RAM and PROM",
+            "4. To initialize parallel port"
+        ]
+    },    
+    // {
+    //     numb:5,
+    //     question:"Which of the following comes under the indexed addressing mode?",
+    //     answer:"4)MOVC @A+DPTR,A",
+    //     options:[
+    //         "1)MOVX A,@DPTR",
+    //         "2)MOV @R0,A",
+    //         "3)MOV A,R0",
+    //         "4)MOVC @A+DPTR,A"
+    //     ]
+    // },    
+
+    
+]
